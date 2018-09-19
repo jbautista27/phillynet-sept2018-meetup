891647e7a448cbd5a4b92abdda37796abe979606
mikem <mike.melusky@gmail.com>
2018-09-19 18:54:30 -0400

    feature complete

--- XyzApi/XyzApi/Repository/Impl/WidgetRepository.cs
+++ XyzApi/XyzApi/Repository/Impl/WidgetRepository.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Dapper;
+using Microsoft.AspNetCore.Builder;
+using SQLitePCL;
 using XyzApi.Models;
 using XyzApi.Services.Impl;
 using XyzApi.Utility;
@@ -18,12 +20,33 @@ namespace XyzApi.Repository.Impl
 
         public async Task<IEnumerable<Widget>> GetWidgets()
         {
-            throw new System.NotImplementedException();
+            using (var sqlConnection = _connectionFactory.GetConnection())
+            {
+                sqlConnection.Open();
+                return await sqlConnection.QueryAsync<Widget>($"SELECT [WidgetId], [WidgetName] FROM [Widget]");
+            }
         }
 
         public async Task AddWidget(string widgetName)
         {
-            throw new System.NotImplementedException();
+            using (var sqlConnection = _connectionFactory.GetConnection())
+            {
+                sqlConnection.Open();
+                await sqlConnection.ExecuteAsync("INSERT INTO [Widget] (WidgetName) VALUES (@WidgetName)",
+                    new
+                    {
+                        WidgetName = widgetName
+                    });
+            }
+        }
+
+        public async Task CreateSqliteSchema(string script)
+        {
+            using (var sqlConnection = _connectionFactory.GetConnection())
+            {
+                sqlConnection.Open();
+                await sqlConnection.ExecuteAsync(script);
+            }
         }
     }
 }
